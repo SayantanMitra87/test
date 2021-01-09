0234864ed0fe1deaedb7f828e71b9e1488fa0cf3
Sayantan Mitra <mitra@enventure.org>
2021-01-09 14:47:10 -0800

    fries

--- hungry.py
+++ hungry.py
@@ -1,10 +1,8 @@
-print("I'm Hungry")
-
-
 hungry=input("are you hungry?")
 if hungry=='yes':
     print('eat samosa')
     print('eat pizza')
     print('eat burger')
+    print('eat fries')
 else:
-    print('Do your homework')
+    print('do your homework')
